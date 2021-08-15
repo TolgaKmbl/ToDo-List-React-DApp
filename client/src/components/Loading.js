99d6ad85f07a448c4c28be7a70cd41071d7aace0
Tolga Kumbul <61317042+TolgaKmbl@users.noreply.github.com>
2021-08-15 19:08:34 +0300

    fixes

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -6,9 +6,7 @@ function Loading() {
     return (  
         <>   
             <h3 id="loading_title" className="text-primary">Loading... Please Wait Patiently!</h3>   
-            <div className="spinner-border text-primary" role="status" id="loading">
-                <span className="sr-only">Loading...</span>            
-            </div>
+            <div className="spinner-border text-primary" role="status" id="loading"></div>
         </>
     )
 }
